0af09680a505d849794e0ef46e95ad18d580671b
ariw96 <ariw96@gmail.com>
2022-05-31 11:59:37 +0300

    enter condition

--- src/components/KeyBoardLetter.jsx
+++ src/components/KeyBoardLetter.jsx
@@ -3,18 +3,27 @@ import { GameContext } from "./GameContext";
 import { useContext } from "react";
 
 function KeyBoardLetter({ letter }) {
-	const { boardState, setBoardState, curLetterPosition, curWordPosition,setCurLetterPosition,setCurWordPosition } =
-		useContext(GameContext);
+	const {
+		boardState,
+		setBoardState,
+		curLetterPosition,
+		curWordPosition,
+		setCurLetterPosition,
+		setCurWordPosition,
+	} = useContext(GameContext);
 	const displayLetter = () => {
-    const newBoardState = [...boardState]
-    newBoardState[curWordPosition][curLetterPosition] = letter
-    setBoardState(newBoardState)
-	 setCurLetterPosition(curLetterPosition + 1)
-	if (curLetterPosition === 4) {
-		setCurWordPosition(curWordPosition + 1)
-		setCurLetterPosition(0)
+		if (letter ==="Ent"){
+		if (curLetterPosition > 4){
+			setCurWordPosition(curWordPosition + 1)
+			setCurLetterPosition(0)
+		}
+		return
 	}
-  };
+		const newBoardState = [...boardState];
+		newBoardState[curWordPosition][curLetterPosition] = letter;
+		setBoardState(newBoardState);
+		setCurLetterPosition(curLetterPosition + 1);
+	};
 	return (
 		<div className="key" onClick={displayLetter}>
 			{letter}
